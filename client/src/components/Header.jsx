c3ef2154d1686a4291d482ed5dc224c1799c321c
lukasz26671 <lukasz26671@wp.pl>
2022-02-08 23:26:40 +0100

    Minor fix for easier dev.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Header(props) {
     return (
@@ -7,8 +8,8 @@ export default function Header(props) {
                 <h2 id="logo">DLZ Studios {props?.additional}</h2>
                 <div className="nav-links">
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="./contact">Contact</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="./contact">Contact</Link></li>
                     </ul>
                 </div>
             </nav>
