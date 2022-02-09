5d9d219b3620d21afde35153dfb01a34430f4c9a
lukasz26671 <lukasz26671@wp.pl>
2022-02-09 11:15:23 +0100

    better?

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -7,6 +7,7 @@ export default function Card(props) {
             <div className="info">
                 <h2 className="title">{props.title ?? "Title"}</h2>
                 {props.children ?? "Description"}
+                <p>{props.link ? <a href={props.link} className="link">Project link</a> : null}</p>
             </div>
             <div className="image">
                 {props.img ? <img className="img" src={`${props.img}`}></img> : ""}
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Card from "./Card.jsx";
 import logo from "../img/logo.jpg";
+import exocraft from "../img/exocraft.jpg";
+import mydb from "../img/mydb.png";
 
 export default function Projects(props) {
     return (
         <div className="projects">
-            <Card class="hero" img={logo} title="FiveInfo">
+            <Card class="hero" img={logo} title="FiveInfo" link="https://github.com/lukasz26671/FiveInfo">
                 Applicaton that displays info about given FiveM server
             </Card>
-            <Card class="hero" img={logo} title="MyDB">
+            <Card class="hero" img={mydb} title="MyDB">
                 A mysql / mongodb wrapper for node.js
 
                 <p><b>IN CONSTRUCTION</b></p>
             </Card>
-            <Card class="hero" img={logo} title="Exocraft">
+            <Card class="hero" img={exocraft} title="Exocraft" link="https://github.com/lukasz26671/Exocraft">
                 Next-gen minecraft mod that expands your modding experience with support for many mods!
             </Card>
-            <Card class="hero" img={logo} title="Exocraft">
-                Next-gen minecraft mod that expands your modding experience with support for many mods!
+            <Card class="hero" img={logo} title="This webpage" link="https://github.com/lukasz26671/DLZStudiosWebpage">
+                Made using React & Sass, with webpack support, allows you to see this website!
             </Card>
         </div>
     );
